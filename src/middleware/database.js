ee080ab86e4a9bfb9562622807302e7bff010206
tahanh <tathimyhanh03@gmail.com>
2020-05-04 18:06:48 +0700

    update 5/4/20

--- src/middleware/database.js
+++ src/middleware/database.js
@@ -1,20 +1,35 @@
-import { MongoClient } from 'mongodb';
-import nextConnect from 'next-connect';
+import { MongoClient } from 'mongodb'
+import nextConnect from 'next-connect'
 
-const client = new MongoClient('{mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass%20Community&ssl=false}', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+const client = new MongoClient(
+  'mongodb://localhost:27017/?3t.uriVersion=2&3t.connectionMode=direct&readPreference=primary&3t.connection.name=tezt',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+)
+// const client = new MongoClient('mongodb://localhost:27017/tezt', function (err, db) {
+//   if (err) {
+//     console.log('Unable to connect to the mongoDB server. Error:', err)
+//     // rejects(err)
+//   } else {
+//     console.log('Connection established to')
+
+//     //Close connection
+//     //db.close();
+//     // resolve(db)
+//   }
+// })
 
 async function database(req, res, next) {
-  if (!client.isConnected()) await client.connect();
-  req.dbClient = client;
-  req.db = client.db('user');
-  return next();
+  if (!client.isConnected()) await client.connect()
+  req.dbClient = client
+  req.db = client.db('db-name')
+  return next()
 }
 
-const middleware = nextConnect();
+const middleware = nextConnect()
 
-middleware.use(database);
+middleware.use(database)
 
-export default middleware;
+export default middleware
