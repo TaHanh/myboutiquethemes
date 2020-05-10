f18a4687ff96e479eb1553905c52eb25c57041a3
TaHanh <tathimyhanh03@gmail.com>
2020-05-10 21:56:32 +0700

    update 10/5/20

--- src/pages/post.js
+++ src/pages/post.js
@@ -1,9 +1,35 @@
 import WysiwygEditor from '../components/wysiwyg-editor'
 import Layout from '../components/layout'
 import '../static/styles/post.scss'
+import { useState } from 'react'
 function Create(props) {
+  const [imgSrc,setImg]= useState("")
+  const [file,setFile]= useState([])
+
+  const change = (file) => {
+
+    var reader = new FileReader();
+    var url = reader.readAsDataURL(file);
+   console.log(url) // Would see a path?
+     reader.onloadend = function (e) {
+       console.log(reader.result)
+        setImg(reader.result)
+      }.bind(this);
+   
+    // TODO: concat files
+  }
   return (
     <Layout title={'Blush'}>
+       <form>
+      <input 
+      
+        type="file" 
+  
+   
+        onChange={e => change(e.target.files[0])}/>
+     </form>
+    {/* Only show first image, for now. */}
+    {imgSrc ? <img src={imgSrc} /> : null}
       <WysiwygEditor />
     </Layout>
   )
